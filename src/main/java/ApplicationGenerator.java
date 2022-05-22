ce3ce431df52a86ff186aa60e93cfd6af9432604
James ZHOU <moverinfo@gmail.com>
2022-05-22 20:16:24 +0800

    Update ApplicationGenerator.java.

--- src/main/java/ApplicationGenerator.java
+++ src/main/java/ApplicationGenerator.java
@@ -4,7 +4,7 @@ import org.tinystruct.data.tools.Generator;
 import org.tinystruct.data.tools.MySQLGenerator;
 
 public class ApplicationGenerator {
-	public static void main(String[]arguments)
+	public static void main(String[] arguments)
 	{
 		System.out.println("Please type the command like this:ApplicationGenerator className tableName");
 		try {
@@ -13,9 +13,9 @@ public class ApplicationGenerator {
 			for(String className:list)
 			{
 				Generator generator=new MySQLGenerator();
-				generator.setFileName("resources/custom/objects/");
+				generator.setFileName("src/main/java/custom/objects/");
 				generator.setPackageName("custom.objects");
-				generator.importPackages("java.util.LocalDateTime");
+				generator.importPackages("java.time.LocalDateTime");
 				generator.create(className,className);
 				System.out.println("class:"+className+" table:"+className);
 			}
