1a039b2bef342880b22fa642c0621df03c4a556b
James ZHOU <moverinfo@gmail.com>
2022-06-22 23:21:49 +0800

    Fixed the issue for sitemap.xml.

--- src/main/java/custom/application/help.java
+++ src/main/java/custom/application/help.java
@@ -17,109 +17,113 @@ package custom.application;
 
 import org.tinystruct.AbstractApplication;
 import org.tinystruct.ApplicationException;
-import org.tinystruct.application.Action;
+import org.tinystruct.http.Response;
+import org.tinystruct.http.ResponseHeaders;
 import org.tinystruct.system.ApplicationManager;
 import org.tinystruct.system.cli.CommandLine;
 import org.tinystruct.system.template.DefaultTemplate;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 
 import static org.tinystruct.handler.DefaultHandler.HTTP_RESPONSE;
 
 public class help extends AbstractApplication {
 
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		this.setAction("help", "privacy");
-		this.setAction("help/condition", "condition");
-		this.setAction("sitemap.xml", "sitemap");
-	}
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        this.setAction("help", "privacy");
+        this.setAction("help/condition", "condition");
+        this.setAction("sitemap.xml", "sitemap");
+    }
 
-	@Override
-	public void setLocale(Locale locale) {
-		super.setLocale(locale);
+    @Override
+    public void setLocale(Locale locale) {
+        super.setLocale(locale);
 
-		this.setText("page.condition.title");
-		this.setText("application.title");
-		this.setText("application.language.name");
+        this.setText("page.condition.title");
+        this.setText("application.title");
+        this.setText("application.language.name");
 
-		this.setText("page.welcome.caption");
-		this.setText("page.language-setting.title");
-		this.setText("page.logout.caption");
+        this.setText("page.welcome.caption");
+        this.setText("page.language-setting.title");
+        this.setText("page.logout.caption");
 
-		this.setText("navigator.bible.caption");
-		this.setText("navigator.video.caption");
-		this.setText("navigator.document.caption");
-		this.setText("navigator.reader.caption");
-		this.setText("navigator.controller.caption");
-		this.setText("navigator.help.caption");
+        this.setText("navigator.bible.caption");
+        this.setText("navigator.video.caption");
+        this.setText("navigator.document.caption");
+        this.setText("navigator.reader.caption");
+        this.setText("navigator.controller.caption");
+        this.setText("navigator.help.caption");
 
-		this.setText("footer.report-a-site-bug");
-		this.setText("footer.privacy");
-		this.setText("footer.register");
-		this.setText("footer.api");
-		this.setText("footer.updates-rss");
-	}
+        this.setText("footer.report-a-site-bug");
+        this.setText("footer.privacy");
+        this.setText("footer.register");
+        this.setText("footer.api");
+        this.setText("footer.updates-rss");
+    }
 
-	public void privacy() throws ApplicationException{
-		InputStream in = help.class.getClassLoader().getResourceAsStream("themes/privacy.view");
-		this.setTemplate(new DefaultTemplate(this, in));
-	}
-	
-	public void condition() throws ApplicationException{
-		InputStream in = help.class.getClassLoader().getResourceAsStream("themes/condition.view");
-		this.setTemplate(new DefaultTemplate(this, in));
-	}
-	
-	public Object sitemap(){
-		lection lection = (custom.application.lection) ApplicationManager.get(custom.application.lection.class.getName());
-		lection.init();
-		Map<String, CommandLine> commandLines = lection.getCommandLines();
-		Set<String> list = commandLines.keySet();
-		StringBuffer buffer=new StringBuffer();
-		buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">");
-		buffer.append("<url>\r\n");
-		buffer.append("  <loc>http://www.ingod.today/</loc>\r\n");
-		buffer.append("  <changefreq>daily</changefreq>\r\n");
-		buffer.append("  <priority>1.00</priority>\r\n");
-		buffer.append("</url>\r\n");
+    public void privacy() throws ApplicationException {
+        InputStream in = help.class.getClassLoader().getResourceAsStream("themes/privacy.view");
+        this.setTemplate(new DefaultTemplate(this, in));
+    }
 
-		buffer.append("<url>\r\n");
-		buffer.append("  <loc>http://www.ingod.today/bible.pdf</loc>\r\n");
-		buffer.append("  <changefreq>daily</changefreq>\r\n");
-		buffer.append("  <priority>0.90</priority>\r\n");
-		buffer.append("</url>\r\n");
-		
-		String path;
-		Iterator<String> iterator = list.iterator();
-		while(iterator.hasNext()) {
-			path=iterator.next();
-			if(path.equals("#") || this.getLink(path).equals("#")) continue;
-			buffer.append("<url>\r\n");
-			buffer.append("  <loc>"+this.getLink(path).replace("&", "&amp;")+"</loc>\r\n");
-			buffer.append("  <changefreq>weekly</changefreq>\r\n");
-			buffer.append("  <priority>0.80</priority>\r\n");
-			buffer.append("</url>\r\n");
-		}
-		
-		buffer.append("</urlset>");
-		
-		HttpServletResponse response = (HttpServletResponse) this.context
-		.getAttribute(HTTP_RESPONSE);
+    public void condition() throws ApplicationException {
+        InputStream in = help.class.getClassLoader().getResourceAsStream("themes/condition.view");
+        this.setTemplate(new DefaultTemplate(this, in));
+    }
 
-		response.setContentType("text/xml;charset="
-		+ this.config.get("charset"));
+    public Object sitemap() {
+        lection lection = (custom.application.lection) ApplicationManager.get(custom.application.lection.class.getName());
+        lection.init();
+        Map<String, CommandLine> commandLines = lection.getCommandLines();
+        Set<String> list = commandLines.keySet();
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">");
+        buffer.append("<url>\r\n");
+        buffer.append("  <loc>http://www.ingod.today/</loc>\r\n");
+        buffer.append("  <changefreq>daily</changefreq>\r\n");
+        buffer.append("  <priority>1.00</priority>\r\n");
+        buffer.append("</url>\r\n");
 
-		return buffer;
-	}
+        buffer.append("<url>\r\n");
+        buffer.append("  <loc>http://www.ingod.today/bible.pdf</loc>\r\n");
+        buffer.append("  <changefreq>daily</changefreq>\r\n");
+        buffer.append("  <priority>0.90</priority>\r\n");
+        buffer.append("</url>\r\n");
 
-	@Override
-	public String version() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        String path;
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            path = iterator.next();
+            if (path.equals("#") || this.getLink(path).equals("#")) continue;
+            buffer.append("<url>\r\n");
+            buffer.append("  <loc>" + this.getLink(path).replace("&", "&amp;") + "</loc>\r\n");
+            buffer.append("  <changefreq>weekly</changefreq>\r\n");
+            buffer.append("  <priority>0.80</priority>\r\n");
+            buffer.append("</url>\r\n");
+        }
+
+        buffer.append("</urlset>");
+
+        Response response = (Response) this.context
+                .getAttribute(HTTP_RESPONSE);
+
+        ResponseHeaders headers = new ResponseHeaders(response);
+        headers.add(org.tinystruct.http.Header.CONTENT_TYPE.set("text/xml;charset="
+                + this.config.get("charset")));
+
+        return buffer;
+    }
+
+    @Override
+    public String version() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
